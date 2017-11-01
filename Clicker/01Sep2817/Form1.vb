e6dfbe1b6461a8e045aa94d7c9ac38d5525d1aeb
Omar Navarro <14omar1414@gmail.com>
2017-11-01 14:40:05 -0400

    Simple clicker game

---
+++ Clicker/01Sep2817/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Dim score As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        add(score)
+        Label1.Text = "your score is : " & score
+    End Sub
+    Public Sub add(ByRef x As Integer)
+        x += 1
+    End Sub
+End Class
---
+++ Clicker/01Sep2817/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("01Sep2817")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("01Sep2817")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("72951b24-9bda-4fdd-a2cc-7ca0597f48fe")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
